3cd42ee721ffe85c00ba8a3c6abe290e34194b36
sadmachine <austin.fishbaugh@gmail.com>
2020-06-04 08:41:16 -0400

    Updated

--- backlog.cpp
+++ backlog.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
+short handle_args(int argc, char ** argv) {
+  if (argc > 1) {
+    if (argv[1] == "config") {
+      cout << "Time to configurate, mate." << endl;
+    } else {
+      cout << "Dunno 'bout that." << endl;
+    }
+  } else if (argc > 2) {
+
+  } else if (argc > 3) {
+
+  } else if (argc > 4) {
+
+  }
+  return 1;
+}
+
 int main(int argc, char ** argv) {
-
-
+  short handle_args_status = handle_args(argc, argv);
   return 0;
 }
--- controller/cli.hpp
+++ controller/cli.hpp
@@ -1,6 +1,14 @@
 #ifndef _CLI_HPP_
 #define _CLI_HPP_
 
+class CLI {
+  private:
+    char ** _arguments;
+    char * _currentArgument;
+    char * _nextArgument;
+    int _nCurrentArgument;
+    int _nNextArgument;
 
+};
 
 #endif
